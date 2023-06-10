607cf203ae5c8bf498354169ba9a52fb07327af7
Jared Ledbetter <jared.ledbetter@outlook.com>
2023-06-09 22:19:31 -0400

    update

--- next.config.js
+++ next.config.js
@@ -21,54 +21,44 @@ const nextConfig = {
   async redirects() {
     return [
       {
-        source: '/:path/',
-        permanent: false,
-        destination: '/:path'
+        source: '/verify',
+        destination:
+          'https://forms.office.com/pages/responsepage.aspx?id=2HI0GalSLEqs4adO-VsnePJuaraJWetIkCKf4FnJyPVUREpWSzNWVDg2OE9MVjJQRTlJN1MxUjFTUy4u',
+        permanent: true
       },
-      {
-        source: '/ama',
-        destination: 'https://us02web.zoom.us/j/82299652955?pwd=MVpxNVd2VjdpWVo5R3lPMGNPTFVCZz09&from=addon ',
-        permanent: false
-      },
-      {
-        source: '/ama-signup',
-        destination: 'https://share.hsforms.com/1N-uouXYHR6CNsq73I0l7Sw4x2gt',
-        permanent: false
-      },
+      { source: '/feedback', destination: 'https://roadmap.veterandb.com', permanent: true },
+      { source: '/roadmap', destination: 'https://roadmap.veterandb.com', permanent: true },
+      { source: '/support', destination: 'https://support.veterandb.com', permanent: true },
+      { source: '/sitemap', destination: '/sitemap.xml', permanent: true },
+      { source: '/sitemaps.xml', destination: '/sitemap.xml', permanent: true },
       { source: '/businesses', destination: '/business', permanent: true },
+      { source: '/directories', destination: '/directory', permanent: true },
+      { source: '/non-profit', destination: '/nonprofit', permanent: true },
+      { source: '/podcasts', destination: '/podcast', permanent: true },
       {
         source: '/businesses/:slug',
         destination: '/business/:slug',
         permanent: true
       },
-      { source: '/directories', destination: '/directory', permanent: true },
       {
         source: '/directories/:slug',
         destination: '/directory/:slug',
         permanent: true
       },
-      { source: '/feedback', destination: 'https://roadmap.veterandb.com', permanent: true },
-      { source: '/non-profit', destination: '/nonprofit', permanent: true },
       {
         source: '/non-profit/:slug',
         destination: '/nonprofit/:slug',
         permanent: true
       },
-      { source: '/podcasts', destination: '/podcast', permanent: true },
       {
         source: '/podcasts/:slug',
         destination: '/podcast/:slug',
         permanent: true
       },
-      { source: '/roadmap', destination: 'https://roadmap.veterandb.com', permanent: true },
-      { source: '/sitemap', destination: '/sitemap.xml', permanent: true },
-      { source: '/sitemaps.xml', destination: '/sitemap.xml', permanent: true },
-      { source: '/support', destination: 'https://support.veterandb.com', permanent: true },
       {
-        source: '/verify',
-        destination:
-          'https://forms.office.com/pages/responsepage.aspx?id=2HI0GalSLEqs4adO-VsnePJuaraJWetIkCKf4FnJyPVUREpWSzNWVDg2OE9MVjJQRTlJN1MxUjFTUy4u',
-        permanent: true
+        source: '/:path/',
+        permanent: false,
+        destination: '/:path'
       }
     ];
   },
