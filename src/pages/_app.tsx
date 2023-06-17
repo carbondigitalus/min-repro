54d659008a7916f699ccd034d20694ffeaf99e69
Jared Ledbetter <jared.ledbetter@outlook.com>
2023-06-16 22:21:45 -0400

    add font-awesome config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,20 @@
 // NPM Modules
+import { config } from '@fortawesome/fontawesome-svg-core';
 import React from 'react';
 import type { AppProps } from 'next/app';
 
+// NPM Styles
+import '@fortawesome/fontawesome-svg-core/styles.css';
+
 // Custom Modules
-import '@/assets/scss/style.scss';
 import { DefaultSeo } from 'next-seo';
 import SEOConfig from '@/../seo.config';
+
+// Custom Styles
+import '@/assets/scss/style.scss';
+
+// Font Awesome config
+config.autoAddCss = false;
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
